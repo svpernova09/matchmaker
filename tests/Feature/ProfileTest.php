38421cc6aa5cb88808ef9d13e5e7a6b27f40b7df
GlennKimbleJr <glennkimble@yahoo.com>
2017-04-25 23:18:52 -0400

    Some cleanup on test and views

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Photo;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
@@ -135,5 +137,35 @@ class ProfileTest extends TestCase
 			->withExceptionHandling()
 			->post('profile/edit', ['gender' => 'unicorn'])
 			->assertSessionHasErrors('gender');
+	}
+
+    /** @test */
+    function unauthenticated_users_cannot_upload_images()
+    {
+        $this->setExpectedException('Illuminate\Auth\AuthenticationException');
+
+        $this->json('POST', '/photos', []);
+    }
+
+	/** @test */
+	function authenticated_users_can_upload_a_photo()
+	{
+        $this->signIn($user = create('App\User'));
+
+        Storage::fake('local');
+
+        $response = $this->json('POST', 'photos', [
+            'photo' => UploadedFile::fake()->image('avatar.png')
+        ]);
+
+        $this->assertEquals(1, $user->photos->count());
+
+        Storage::assertExists(
+            'photos/' . $user->photos[0]->photo_path
+        );
+
+        Storage::assertExists(
+            'thumbnails/' . $user->photos[0]->photo_path
+        );
 	}
 }
